20794d1055952241e331df7e233ab0fa5f05cc64
Gabriel <gabriel.such@hotmail.com>
2022-04-21 22:16:41 -0300

    upgraded

--- app/configs/database.py
+++ app/configs/database.py
@@ -9,6 +9,7 @@ db = SQLAlchemy()
 def init_app(app: Flask):
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
 
